29009cf5e0fbed2c9228b0e05fab85157bf2ddb0
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-05-26 11:55:03 +0300

    Example6 added

---
+++ Example6_While/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 8;
+int b = 4;
+int c = 9;
+int d = 2;
+int e = 1;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максимальное число: ");
+Console.Write(max);
